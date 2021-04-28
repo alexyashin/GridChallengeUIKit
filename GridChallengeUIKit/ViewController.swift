c8c7d524c6fe445b0a027e47c1a91e1fbc7c1c4b
Alexey Yashin <ensiferumorl@gmail.com>
2021-04-28 16:23:22 +0300

    Initial Commit

---
+++ GridChallengeUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GridChallengeUIKit
+//
+//  Created by Alexey Yashin on 28.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
